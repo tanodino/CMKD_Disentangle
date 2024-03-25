663d0be01c62f3653795471b3f32cfd26cf497e4
tanodino <dino@Dinos-MacBook-Pro.local>
2024-03-25 09:56:28 +0100

    bug

--- launch_ourV2_TRISTART.sh
+++ launch_ourV2_TRISTART.sh
@@ -2,7 +2,7 @@ for i in $(seq 0 1 9)
 do
     for k in CONTRA ORTO
     do
-        python main_cs.py TRISTAR DEPTH THERMAL $i $k
-        python main_cs.py TRISTAR THERMAL DEPTH $i $k
+        python main_cmkd_v2.py TRISTAR DEPTH THERMAL $i $k
+        python main_cmkd_v2.py TRISTAR THERMAL DEPTH $i $k
     done
 done
