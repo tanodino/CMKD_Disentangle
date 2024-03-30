e285a30216f3b7259a42b99baa4eb6b609045b77
tanodino <dino@Dinos-MacBook-Pro.local>
2024-03-30 23:41:01 +0100

    data augmentation, only geometrical ones

--- monoSource.py
+++ monoSource.py
@@ -20,7 +20,7 @@ def createDataLoader(x, y, tobeshuffled, BATCH_SIZE, type_data='none'):
     x_tensor = torch.tensor(x, dtype=torch.float32)
     y_tensor = torch.tensor(y, dtype=torch.int64)
     dataset = None
-    if type_data == 'RGB' or type_data=='MS' or type_data=='MNIST'  or type_data=='SAR':
+    if type_data == 'RGB' or type_data=='MS' or type_data=='MNIST' or type_data=='SAR' or type_data=='DEPTH' or type_data=='THERMAL':
         dataset = MyDataset(x_tensor, y_tensor, transform=transform)
     else:
         dataset = TensorDataset(x_tensor, y_tensor)
