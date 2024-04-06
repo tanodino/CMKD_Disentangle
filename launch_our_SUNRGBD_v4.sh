f7e209e2f4aed65607235802006c7db3cfd84ee5
tanodino <dino@Dinos-MacBook-Pro.local>
2024-04-06 14:23:40 +0200

    modif sh sunrgbd v4

--- launch_our_SUNRGBD_v4.sh
+++ launch_our_SUNRGBD_v4.sh
@@ -2,8 +2,8 @@ for i in $(seq 0 1 4)
 do
     for k in CONTRA #ORTO
     do
-        python main_cs_v3.py SUNRGBD RGB DEPTH $i $k
-        python main_cs_v3.py SUNRGBD DEPTH RGB $i $k
+        python main_cs_v4.py SUNRGBD RGB DEPTH $i $k > log_v4_SUNRGBD_RGB_DEPTH_${i}_${k}
+        python main_cs_v4.py SUNRGBD DEPTH RGB $i $k > log_v4_SUNRGBD_DEPTH_RGB_${i}_${k}
         #python main_cs.py SUNRGBD RGB DEPTH $i $k
         #python main_cs.py SUNRGBD DEPTH RGB $i $k
     done
