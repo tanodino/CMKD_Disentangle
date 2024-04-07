8bbd48d290f0c78049440b1fac9ad92dc51a6141
tanodino <dino@Dinos-MacBook-Pro.local>
2024-04-07 23:50:14 +0200

    restor v4Compact

--- main_cv_v4_compact.py
+++ main_cv_v4_compact.py
@@ -318,7 +318,7 @@ for epoch in range(EPOCHS):
         loss_adv_dann = loss_fn( tot_pred_adv, y_dom )    
         
         
-        loss = loss_pred + loss_pred_dom + loss_adv_dann + loss_contra + loss_ortho
+        loss = loss_pred + loss_pred_dom  + loss_contra + loss_ortho #+ loss_adv_dann
 
         
         '''
