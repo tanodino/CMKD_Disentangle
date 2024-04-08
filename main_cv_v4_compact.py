79ce994c0a3d428b32410d1d910ba8ef6e694cb7
tanodino <dino@Dinos-MacBook-Pro.local>
2024-04-08 20:25:07 +0200

    modif iterator combi

--- main_cv_v4_compact.py
+++ main_cv_v4_compact.py
@@ -333,7 +333,8 @@ for epoch in range(EPOCHS):
         tot_pred_adv = torch.cat([discr_f, discr_s])
         loss_adv_dann = loss_fn( tot_pred_adv, y_dom )    
 
-        emb_scl_cl = nn.functional.softmax( tot_pred, dim=1 )
+        #emb_scl_cl = nn.functional.softmax( tot_pred, dim=1 )
+        emb_scl_cl = nn.functional.normalize(tot_pred)
         loss_contra_cl = scl( emb_scl_cl , y_scl )
         
         loss = loss_pred + loss_adv_dann + loss_pred_dom + loss_contra + loss_contra_cl
